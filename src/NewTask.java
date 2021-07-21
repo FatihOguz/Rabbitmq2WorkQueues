be28a95ce55c18a051fed80ad410e6267bf83a16
fatih oğuz <foguz4999@gmail.com>
2021-07-22 00:55:47 +0300

    Initial commit

---
+++ src/NewTask.java
@@ -0,0 +1,39 @@
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.MessageProperties;
+
+import java.util.Random;
+
+public class NewTask {
+
+    private static final String TASK_QUEUE_NAME = "task_queue";
+
+    public static void main(String[] argv) throws Exception {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        try (Connection connection = factory.newConnection();
+             Channel channel = connection.createChannel()) {
+            channel.queueDeclare(TASK_QUEUE_NAME, true, false, false, null);
+
+            Random r=new Random(); //random sınıfı
+            int a=r.nextInt(10);
+
+            while(1==1){
+                a=r.nextInt(10);
+                String fakeArgv = String.valueOf(a);
+
+
+                //String message = String.join(" ", argv);
+                String message = String.join(" ", fakeArgv);
+
+                channel.basicPublish("", TASK_QUEUE_NAME,
+                        MessageProperties.PERSISTENT_TEXT_PLAIN,
+                        message.getBytes("UTF-8"));
+                System.out.println(" [x] Sent '" + message + "'");
+            }
+
+        }
+    }
+
+}
